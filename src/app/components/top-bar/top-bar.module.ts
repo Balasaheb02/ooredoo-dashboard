5fbbff7ecc7db0fc4d02ea16643db88c4729b1a8
Balasaheb02 <thakarebalasaheb02@gmail.com>
2020-11-21 00:16:41 +0530

    global style and images imported

---
+++ src/app/components/top-bar/top-bar.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopBarComponent } from './top-bar.component';
+
+
+
+@NgModule({
+  declarations: [TopBarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TopBarModule { }
